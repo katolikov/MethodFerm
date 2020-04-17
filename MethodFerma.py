a79983fad5aa411fa1de5243d8fdf52c0b659a5c
katolikov <55280831+katolikov@users.noreply.github.com>
2020-04-17 23:12:18 +0300

    Add files via upload

---
+++ MethodFerma.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import gmpy2
+import math
+import time
+from datetime import datetime
+from gmpy2 import mpq, mpz, sqrt, ceil
+from sys import argv
+
+def square(x,n):
+    k = 1
+    while k > 0:
+        y = gmpy2.sqrt((mpq(x) + mpq(k))**2 - mpq(n))
+        if gmpy2.is_integer(y):
+           return y, k
+        else:
+           k+=1
+
+def MethodFerma(n):
+    x = ceil(sqrt(n))
+    y, k = square(x,n)
+    a = mpz(x) + mpz(k) + mpz(y)
+    b = mpz(x) + mpz(k) - mpz(y)
+    return a, b, k
+
+if __name__ == '__main__':
+  if len (argv) == 2:
+       if int(argv[1]) % 2 != 0:
+           start = datetime.now()
+           a, b, k = MethodFerma(mpz(argv[1]))
+           end = datetime.now() - start
+           print("\n[ Factor: a + b = ", mpz(a), "] \n")
+           print("[ Factor: a - b = ", mpz(b), "] \n")
+           print("[ Count: k = ", mpz(k), "] \n")
+           print("Time: ", end)
+
+       else:
+           print("[Error] Your number is even")
+
+  else:
+       print("[Error] No input number")
